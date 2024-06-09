7b7205267e04d2636153d748ca4114b12a23bbc7
arpita jadhav <50541757+Arpita24k@users.noreply.github.com>
2024-06-08 19:54:40 -0500

    Add files via upload

---
+++ best_time_to buy_and_sell.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        Calculate the maximum profit that can be obtained by buying and selling a stock multiple times.
+        
+        Args:
+        - prices: List of stock prices on each day
+        
+        Returns:
+        - Maximum profit
+        """
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        return max_profit
+
+# Example usage
+solution = Solution()
+
+prices1 = [7, 1, 5, 3, 6, 4]
+prices2 = [1, 2, 3, 4, 5]
+prices3 = [7, 6, 4, 3, 1]
+
+print("Max Profit for prices1:", solution.maxProfit(prices1))  # Output: 7
+print("Max Profit for prices2:", solution.maxProfit(prices2))  # Output: 4
+print("Max Profit for prices3:", solution.maxProfit(prices3))  # Output: 0
