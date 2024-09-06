2ec5460bc5f438c7d7ba66bd583e68c6a0f40e46
Arpita J <50541757+Arpita24k@users.noreply.github.com>
2024-09-06 18:56:59 -0500

    Add Binary Tree Level Order Traversal II
     Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
    
    Example 1:
    Input: root = [3,9,20,null,null,15,7]
    Output: [[15,7],[9,20],[3]]
    
    Example 2:
    Input: root = [1]
    Output: [[1]]
    
    Example 3:
    Input: root = []
    Output: []
     
    Constraints:
    The number of nodes in the tree is in the range [0, 2000].
    -1000 <= Node.val <= 1000

---
+++ binary_tree_level_order_traversal_ii.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode):
+        # If root is None, return an empty list
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+
+        # Return the result reversed for bottom-up order
+        return result[::-1]
+
+# Example usage:
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+sol = Solution()
+print(sol.levelOrderBottom(root))  # Output: [[15,7],[9,20],[3]]
